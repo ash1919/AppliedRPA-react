77d9242f8c4b952304924504a8ec824cf985ff77
ash1919 <akashjayaraj87@gmail.com>
2022-06-13 11:35:25 +0530

    first commit

---
+++ src/component/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero_bg bg-rpaBlue-900 text-white -mt-[8.8rem] ">
+      <div className="container mx-auto px-6 pb-52 pt-40  max-w-7xl">
+        <div className="flex flex-col lg:flex-row items-center justify-between">
+          <div className="flex flex-col">
+            <h1 className="text-5xl lg:text-6xl font-bold">
+              We're Applied RPA
+            </h1>
+            <p className="text-xl lg:text-2xl text-rpaBlack-300 pt-2">
+              <em>Time is short let's make it even shorter</em>
+            </p>
+          </div>
+          <div>
+            <img src="/images/hero-1.png" alt="hero-img" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
